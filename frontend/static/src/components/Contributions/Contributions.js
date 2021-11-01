d072969cf96e7ee225b37b1410a39789d105c20f
Kat Ingram <69052750+kattyeye@users.noreply.github.com>
2021-11-01 16:00:50 -0400

    Adding contributions js

---
+++ frontend/static/src/components/Contributions/Contributions.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+
+function Contributions() {
+  const [contribs, setContribs] = useState([]);
+  useEffect(() => {
+    async function fetchContribs() {
+      const response = await fetch(`/api_v1/contributions/`);
+      const data = await response.json();
+      console.log("articles", data);
+      setContribs(data);
+    }
+    fetchContribs();
+  }, []);
+  return (
+    <div>
+      {contribs?.map((contribution) => (
+        <div>{contribution.text}</div>
+      ))}
+    </div>
+  );
+}
