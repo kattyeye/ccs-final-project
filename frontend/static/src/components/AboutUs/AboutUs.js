42eb104c9570363f4e0394c3c085b1d0a834bc16
Kat Ingram <69052750+kattyeye@users.noreply.github.com>
2021-11-16 16:44:24 -0500

    Adding styling to navbar toggler

--- frontend/static/src/components/AboutUs/AboutUs.js
+++ frontend/static/src/components/AboutUs/AboutUs.js
@@ -1,37 +1,12 @@
 import { withRouter, NavLink } from "react-router-dom";
 import bg1 from "../App/pics/bg1.jpg";
+import neon from "../App/pics/neonsign.jpg";
+import { FaEnvelope, FaInstagram, FaFacebook, FaTwitter } from "react-icons/fa";
+import { Card } from "@mui/material";
 function AboutUs() {
   return (
-    <div className="container-fluid mb-5">
-      {/* <div className="row d-sm-flex align-items-center justify-content-between">
-        <div className="col-md-6 order-2 order-sm-1 p-3 p-sm-5">
-          <div className="p-0 p-sm-5">
-            <small className="text-uppercase" style={{ color: "#429125" }}>
-              Our Story
-            </small>
-            <h1 className="mb-4 display-4" style={{ fontWeight: "600" }}>
-              How it all <span style={{ color: "#429125" }}>Started</span>.
-            </h1>
-            <p style={{ lineHeight: "2" }}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
-              repellat iure laboriosam cum voluptatum, nam minima deserunt aut?
-              Distinctio voluptatibus dolor quaerat quo omnis illo sequi at
-              velit, odit quod!
-            </p>
-            <button
-              type="button"
-              style={{ background: "none", border: "none" }}
-            >
-              <a href="#footer" className="btn link-button">
-                Contact US
-              </a>
-            </button>
-          </div>
-        </div>
-        <div className="w-50 col-md-6 order-1 order-sm-2 about-us-header-image"></div>
-      </div> */}
-
-      <section className="">
+    <div className="d-flex-md">
+      <section className="container-fluid how-it-started-section">
         <div className="row d-sm-flex align-items-center justify-content-between">
           <div className="col-md-6 order-2 order-sm-1 p-3 p-sm-5">
             <div className="p-0 p-sm-5">
@@ -66,6 +41,123 @@ function AboutUs() {
           </div>
         </div>
       </section>
+
+      {
+        /* <div className="container my-5 text-white calltoaction">
+        <div className="p-5 d-flex flex-column align-items-center justify-content-between">
+          <span className="text-uppercase text-light">Sub Headline</span>
+          <h1 className="mb-4 mt-2 font-weight-bold">Your Headline Here</h1>
+          {/* <div className="d-flex socialicons">
+            <li>
+              <a href="mailto:katherineingram5@icloud.com">
+                <FaEnvelope />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.instagram.com/86hate">
+                <FaInstagram />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.facebook.com/86hate/">
+                <FaFacebook />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.twitter.com/86hate/">
+                <FaTwitter />
+              </a>
+            </li>
+          </div> */
+        // </div>
+        // </div>
+      }
+
+      <div className="py-5 about-how-section container-fluid">
+        <div className="text-center mb-5">
+          <span>JOIN THE FIGHT</span>
+          <h1 className="text-capitalize font-weight-bold">
+            How it <span style={{ color: "#429125" }}>Works.</span>
+          </h1>
+        </div>
+
+        <div className="col-12 col-md-8 mx-auto">
+          <div className="d-flex my-4 align-items-start">
+            <div className="mr-3 text-center mt-2">
+              <div
+                className="p-4 rounded-circle text-white font-weight-bold d-flex align-items-center justify-content-center"
+                style={{
+                  height: "40px",
+                  width: "40px",
+                  backgroundColor: " #429125",
+                }}
+              >
+                1
+              </div>
+            </div>
+            <Card className="rounded about-cards p-4">
+              <h5 className="mb-3" style={{ fontWeight: "600" }}>
+                Your Headline Here!
+              </h5>
+              <p className=" font-weight-light">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptatem aperiam temporibus sequi reprehenderit, maxime at
+                cupiditate officia ipsam ipsum.
+              </p>
+            </Card>
+          </div>
+
+          <div className="d-flex my-4 align-items-start">
+            <div className="mr-3 text-center mt-2">
+              <div
+                className="p-4 rounded-circle text-white font-weight-bold d-flex align-items-center justify-content-center"
+                style={{
+                  height: "40px",
+                  width: "40px",
+                  backgroundColor: " #429125",
+                }}
+              >
+                2
+              </div>
+            </div>
+            <Card className="rounded about-cards p-4">
+              <h5 className="mb-3" style={{ fontWeight: "600" }}>
+                Your Headline Here!
+              </h5>
+              <p className=" font-weight-light">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptatem aperiam temporibus sequi reprehenderit, maxime at
+                cupiditate officia ipsam ipsum.
+              </p>
+            </Card>
+          </div>
+
+          <div className="d-flex my-4 align-items-start">
+            <div className="mr-3 text-center mt-2">
+              <div
+                className="p-4 rounded-circle text-white font-weight-bold d-flex align-items-center justify-content-center"
+                style={{
+                  height: "40px",
+                  width: "40px",
+                  backgroundColor: " #429125",
+                }}
+              >
+                3
+              </div>
+            </div>
+            <Card className="rounded about-cards p-4">
+              <h5 className="mb-3" style={{ fontWeight: "600" }}>
+                Your Headline Here!
+              </h5>
+              <p className=" font-weight-light">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptatem aperiam temporibus sequi reprehenderit, maxime at
+                cupiditate officia ipsam ipsum.
+              </p>
+            </Card>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
